d442579328886d108b0e4c4ff6f84ab658adf96c
bhatnag8 <bhatnag8@purdue.edu>
2022-12-10 18:51:55 -0500

    Initial Commit

---
+++ Spacefy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spacefy
+//
+//  Created by Arryan Bhatnagar on 12/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
