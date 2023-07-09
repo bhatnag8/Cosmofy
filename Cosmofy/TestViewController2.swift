c9c144d26c7722f6f7eeeae7893a98e7c0e7b518
Arryan Bhatnagar <bhatnag8@purdue.edu>
2023-07-09 14:25:26 -0400

    Starting Login Implementation

--- Cosmofy/TestViewController2.swift
+++ Cosmofy/TestViewController2.swift
@@ -10,28 +10,16 @@ import UIKit
 class TestViewController2: UIViewController {
 
     @IBOutlet weak var Logo: UIImageView!
-    @IBOutlet weak var SignIn: UIButton!
     
+    @IBOutlet weak var loginProviderStackView: UIStackView!
+    
+    // MARK: - viewDidLoad()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-
-        // Do any additional setup after loading the view.
-        
-        SignIn.layer.cornerRadius = SignIn.bounds.height / 2
-        
         Logo.layer.cornerRadius = 12
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+ 
 
 }
