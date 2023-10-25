e352931ea62eb12205834696a599030ebeed5851
Arryan Bhatnagar <bhatnag8@purdue.edu>
2023-10-25 13:12:31 -0400

    Clean Attempt #4

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -4,10 +4,10 @@ echo "Stage: PRE-Xcode Build is activated .... "
 
 # Move to the place where the scripts are located.
 # This is important because the position of the subsequently mentioned files depend of this origin.
-cd $CI_WORKSPACE/ci_scripts || exit 1
+cd $CI_PRIMARY_REPOSITORY_PATH/ci_scripts || exit 1
 
 # Write a JSON File containing all the environment variables and secrets.
-printf "{\"STRIPE_KEY\":\"%s\"}" "$STRIPE_KEY" >> ../Cosmofy/Cosmofy/SupportingFiles/secrets.json
+printf "{\"STRIPE_KEY\":\"%s\"}" "$STRIPE_KEY" >> ../Cosmofy/Cosmofy/SupportingFiles/Secrets.json
 
 echo "Wrote secrets.json file."
 
