cd7dcff432267a1fdba6d4b42305332cd812e5bf
Arryan Bhatnagar <bhatnag8@purdue.edu>
2023-10-25 14:55:01 -0400

    Not-so clean Attempt #7

--- Cosmofy/ci_scripts/ci_pre_xcodebuild.sh
+++ Cosmofy/ci_scripts/ci_pre_xcodebuild.sh
@@ -7,7 +7,7 @@ echo "Stage: PRE-Xcode Build is activated .... "
 cd $CI_PRIMARY_REPOSITORY_PATH/ci_scripts || exit 1
 
 # Write a JSON File containing all the environment variables and secrets.
-printf "{\"API_KEY\":\"%s\"}" "$API_KEY" >> ../Cosmofy/SupportingFiles/Secrets.json
+printf "{\"API_KEY\":\"%s\"}" "$API_KEY" >> ../SupportingFiles/Secrets.json
 
 echo "Wrote secrets.json file."
 
